80e39ad4aac7d01ac3ec7bb1bae73a05ace3996b
hainsworth99 <31012794+hainsworth99@users.noreply.github.com>
2019-08-03 17:00:47 -0600

    created fundraisingrecords view and preliminary template

--- fundraisingrecords/views.py
+++ fundraisingrecords/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
+from .models import FundraisingRecord
+
+
 class FundraisingRecordsListView(LoginRequiredMixin, ListView):
-    foo = 0
+    model = FundraisingRecord
+    template_name = 'fundraising_records.html'
