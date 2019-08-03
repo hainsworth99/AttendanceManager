b5db2a5fe207569086178e3e0ea266166c0b3a3b
hainsworth99 <31012794+hainsworth99@users.noreply.github.com>
2019-08-02 21:23:18 -0600

    added training_group to user model

--- users/models.py
+++ users/models.py
@@ -3,10 +3,10 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
-    is_admin = models.BooleanField(default=False)  # boolean that will give some admin privileges to certain users
     student_id = models.IntegerField(null=True, blank=False)
     first_name = models.CharField(_('first name'), max_length=30, blank=False)
     last_name = models.CharField(_('last name'), max_length=150, blank=False)
     email = models.EmailField(_('email address'), blank=False)
+    training_group = models.IntegerField(null=True, blank=True)  # will allow automated training group sorting, 1-4 beginner-advanced
 
     REQUIRED_FIELDS = ['email', 'student_id', 'first_name', 'last_name']
